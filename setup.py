4ef85cf370798ff45c403f72876d4e4422ad87e6
chris48s <chris.shaw480@gmail.com>
2018-03-05 11:14:49 +0000

    add packaging info

---
+++ setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup
+
+
+# allow setup.py to be run from any path
+def _get_description():
+    try:
+        path = os.path.join(os.path.dirname(__file__), 'README.rst')
+        with open(path, encoding='utf-8') as f:
+            return f.read()
+    except IOError:
+        return ''
+
+
+setup(
+    name='uk_geo_utils',
+    version='0.1.0',
+    author="chris48s",
+    license="MIT",
+    url="https://github.com/DemocracyClub/uk-geo-utils",
+    packages=['uk_geo_utils'],
+    description='Django app for working with OS Addressbase, ONSUD and ONSPD',
+    long_description=_get_description(),
+    install_requires=[
+        'Django',
+        'psycopg2-binary',
+    ],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
