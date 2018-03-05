d5fdd44413f144f79e662f05840fa47afcfb4f1f
chris48s <chris.shaw480@gmail.com>
2018-03-05 11:15:55 +0000

    add script to run unit tests

---
+++ run_tests.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import os
+import sys
+import django
+from django.conf import settings
+from django.test.utils import get_runner
+
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+root = lambda *x: os.path.join(BASE_DIR, *x)
+
+if not settings.configured:
+    settings.configure(
+        DEBUG=True,
+        DATABASES={
+            'default': {
+                'ENGINE': 'django.contrib.gis.db.backends.postgis',
+                'NAME': 'test',
+                'USER': 'postgres',
+                'PASSWORD': '',
+                'HOST': '',
+                'PORT': '',
+            }
+        },
+        INSTALLED_APPS=(
+            'uk_geo_utils',
+        ),
+    )
+
+django.setup()
+TestRunner = get_runner(settings)
+test_runner = TestRunner(verbosity=1, interactive=True, failfast=False)
+failures = test_runner.run_tests(['uk_geo_utils', ])
+sys.exit(failures)
