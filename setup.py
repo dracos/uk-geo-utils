2eb919159353eb91a09a6b7226b444fca5b3efcf
GeoWill <roper.william@gmail.com>
2021-01-21 15:54:49 +0000

    Bump version to 0.10.2

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def _get_description():
 
 setup(
     name="uk_geo_utils",
-    version="0.10.1",
+    version="0.10.2",
     author="chris48s",
     license="MIT",
     url="https://github.com/DemocracyClub/uk-geo-utils",
